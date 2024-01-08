50ea0a507acba8b45d69edfa12945360a6969a16
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-01-08 15:04:42 +0100

    MW de contrôle des requêtes

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,33 +1,44 @@
 const routes = (app) => {
+    // routeur de la page de contact     
     app.route('/contact')
-    .get((req, res)=>
-        res.send('GET request successfull !'))
-    .post((req, res)=>
-        res.send('POST request successfull !'));
+        .get((req, res, next)=> {
+            // Middleware
+            console.log(`request from : ${req.originalUrl}`)
+            console.log(`request type : ${req.method}`)
+            next();
+        }, ((req, res, next) => {
+            res.send('GET request successfull !');
+        }))
+
+        .post((req, res, next)=> {
+            // Middleware
+            console.log(`request from : ${req.originalUrl}`)
+            console.log(`request type : ${req.method}`)
+            next();
+        },  ((req, res, next) => {
+            res.send('POST request successfull !');
+        }))    
     
+    // routeur de la page de contact paramétrée par l'id du contact
     app.route('/contact/:contactId')
-    .put((req, res)=>
-        res.send('PUT request successfull !'))
-    .delete((req, res)=>
-        res.send('DELETE request successfull !'));
-        
-        
+        .put((req, res, next)=> {
+            // Middleware
+            console.log(`request from : ${req.originalUrl}`)
+            console.log(`request type : ${req.method}`)
+            next();
+        }, ((req, res, next) => {
+            res.send('PUT request successfull !');
+        }))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        .delete((req, res, next)=> {
+            // Middleware
+            console.log(`request from : ${req.originalUrl}`)
+            console.log(`request type : ${req.method}`)
+            next();
+        },  ((req, res, next) => {
+            res.send('DELETE request successfull !');
+        }))
+  
 }
 
 
