3ce486792cb04f8941a9d4b49fe608240fbd8ec8
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-01-08 15:42:38 +0100

    import mongoose & backup bodyParser

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//! IMPORTS / DECLARATIONS des modules de notre projet
 // Déclarer et initialiser Dotenv
 import dotenv from "dotenv";
 const result = dotenv.config();
@@ -9,6 +10,25 @@ const app = express();
 // Déclarer et importer le router dans l'application express
 import routes from "./src/routes/crmRoutes.js";
 routes(app);
+
+// Importer Mongoose (Object Document Mapper) pour la connexion à la base de données MongoDB
+import mongoose from "mongoose";
+// Déclarer et initialiser la connexion à la base de données MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URI, {
+    // useMongoClient: true, ---> Option deprecated depuis la version 5.0 de Mongoose
+});
+
+// BACKUP - Importer le module body-parser pour analyser les requêtes de formulaire
+// dans le cas ou on ne veut/peux pas utiliser express.urlencoded({ extended: true })
+// import bodyParser from "body-parser";
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
 
 // fonction middleware d'analyse du corps des requêtes de formulaire. (extended: true) permet de parser les objets et les tableaux.
 app.use(express.urlencoded({ extended: true }));
